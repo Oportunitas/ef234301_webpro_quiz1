529716ead54d5e605582eb2850c907e74b1fc4ff
Oportunitas <123826702+Oportunitas@users.noreply.github.com>
2023-10-02 12:13:44 +0700

    updated dynamic pixel parameters

---
+++ dynamic-pixel.js
@@ -0,0 +1,27 @@
+function getDynamicPixelSize() {
+    var pxViewHeight = window.innerHeight;
+    var pxViewWidth = window.innerWidth;
+
+    var scale = Math.min((pxViewHeight / pxViewWidth), (pxViewWidth / pxViewHeight));
+
+    var unit = (pxViewHeight / 10) * (pxViewWidth / 10);
+    unit /= 400;
+
+    var dynamicPixelSize = Math.max(8, (unit * scale));
+
+    console.log(scale);
+    console.log(pxViewHeight / pxViewWidth);
+    console.log(pxViewWidth / pxViewHeight);
+    console.log("-----")
+    return dynamicPixelSize + "px";
+}
+
+var dynamicPixelView = document.getElementById("dynamic-pixel");
+    dynamicPixelView.style.fontSize = getDynamicPixelSize(dynamicPixelView);
+
+function updateDynamicPixelSize() {
+    var dynamicPixelView = document.getElementById("dynamic-pixel");
+    dynamicPixelView.style.fontSize = getDynamicPixelSize(dynamicPixelView);
+}
+
+window.addEventListener('resize', updateDynamicPixelSize);
